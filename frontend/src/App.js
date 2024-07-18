893b8c7de99e88819ff629bf3d37b2dc2b3f1913
PedroLongue <pedrolongue13@gmail.com>
2024-07-18 17:31:13 -0300

    Feat: Add Footer component and NavBar component with css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,10 @@ import "./App.css";
 //Router
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
+//Components
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
 //pages
 import Home from "./pages/Home/Home";
 import Login from "./pages/Auth/Login";
@@ -12,11 +16,15 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/login" element={<Login />}></Route>
-          <Route path="/register" element={<Register />}></Route>
-        </Routes>
+        <Navbar />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/login" element={<Login />}></Route>
+            <Route path="/register" element={<Register />}></Route>
+          </Routes>
+        </div>
+        <Footer />
       </BrowserRouter>
     </div>
   );
--- frontend/src/pages/Auth/Register.js
+++ frontend/src/pages/Auth/Register.js
@@ -1,7 +1,34 @@
 import "./Auth.css";
 
+//components
+import { Link } from "react-router-dom";
+
+//Hooks
+import { useState, useEffect } from "react";
+
 const Register = () => {
-  return <div>Register</div>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div id="register">
+      <h2>ReactGram</h2>
+      <p className="subtitle">Cadastre-se para ver as fotos dos seus amigos.</p>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Nome" />
+        <input type="email" placeholder="E-mail" />
+        <input type="password" placeholder="Senha" />
+        <input type="password" placeholder="Confirme a senha" />
+
+        <input type="submit" value="Cadastrar" />
+      </form>
+
+      <p>
+        Já tem conta? <Link to="/login">Clique aqui.</Link>
+      </p>
+    </div>
+  );
 };
 
 export default Register;
