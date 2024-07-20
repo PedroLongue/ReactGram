18ee359e6cba46a2de724594c0cd0337c69820ee
PedroLongue <pedrolongue13@gmail.com>
2024-07-19 23:10:51 -0300

    Feat: add redux for auth

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// Redux
+import { Provider } from "react-redux"; // compartilha infos entre os componentes
+import { store } from "./store";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}> 
+      <App />
+    </Provider>
   </React.StrictMode>
 );
